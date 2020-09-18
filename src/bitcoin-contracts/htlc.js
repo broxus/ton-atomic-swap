3a508204c5035bb58e6cfe18ec4ca121a9c3d8cd
Maga <steam.30mb1@gmail.com>
2020-09-18 14:05:41 +0300

    Added js scripts for managing HTLC contracts for bitcoin

---
+++ src/bitcoin-contracts/htlc.js
@@ -0,0 +1,73 @@
+const op = require('btc-opcode').map;
+const { decode, encode } = require('./segwit');
+const bitcoin = require('bitcoinjs-lib');
+const bs58check = require('bs58check');
+
+
+const TESTNET_VERSION_PREFIX = 196;
+const OP_CHECKLOCKTIMEVERIFY = 177;
+
+function isEven(num) { return ((num % 2) === 0); }
+
+function addPadding(str, required_len) {
+    return "0".repeat(required_len - str.length) + str;
+}
+
+function toPaddedHexString(num, len) {
+    str = num.toString(16);
+    return addPadding(str, len);
+}
+
+function add_varInt(value) {
+    var value_len = value.length;
+    if (!isEven(value_len)) {
+        value_len += 1;
+    }
+    var padded_value = addPadding(value, value_len);
+    value_len = value_len / 2;
+    if (value_len <= 75) {
+        return toPaddedHexString(value_len, 2) + padded_value;
+    } else if (value_len <= 255) {
+        return toPaddedHexString(op.OP_PUSHDATA1, 2) + toPaddedHexString(value_len, 2) + padded_value;
+    } else if (value_len <= 65535) {
+        return toPaddedHexString(op.OP_PUSHDATA2, 2) + toPaddedHexString(value_len, 4) + padded_value
+    }
+
+    throw `Value too big: ${value_len}`;
+}
+
+function serializeScript(script_arr) {
+    const serialized = script_arr.map((elem) => {
+        if (typeof elem === "number" && elem >= 0 && elem <= 255) {
+            return toPaddedHexString(elem, 2);
+        } else if (typeof elem === 'string') {
+            return add_varInt(elem);
+        } else {
+            throw `Unexpected elem in script: ${elem}`;
+        }
+    });
+
+    return serialized.join('');
+}
+
+function htlc(timelock, payer_addr, receiver_addr, hash_) {
+    var buf = Buffer.allocUnsafe(4);
+    buf.writeUInt32LE(timelock);
+    const timelock_hex = buf.toString('hex');
+    const payer_hashed_pubkey = decode('tb', payer_addr).program.map(i => toPaddedHexString(i, 2)).join('');
+    const receiver_hashed_pubkey = decode('tb', payer_addr).program.map(i => toPaddedHexString(i, 2)).join('');
+
+    var script = [op.OP_SHA256, hash_, op.OP_EQUAL];
+    script = [...script, ...[op.OP_IF, op.OP_DUP, op.OP_HASH160, receiver_hashed_pubkey, op.OP_EQUALVERIFY, op.OP_CHECKSIG]];
+    script = [...script, ...[op.OP_ELSE, timelock_hex, OP_CHECKLOCKTIMEVERIFY, op.OP_DROP]];
+    script = [...script, ...[op.OP_DUP, op.OP_HASH160, payer_hashed_pubkey, op.OP_EQUALVERIFY, op.OP_CHECKSIG, op.OP_ENDIF]];
+
+    return serializeScript(script);
+}
+
+function script_to_p2sh_addr(script) {
+    const hashed_script = TESTNET_VERSION_PREFIX.toString(16) + bitcoin.crypto.hash160(Buffer.from(script, 'hex')).toString('hex');
+    return bs58check.encode(Buffer.from(hashed_script, 'hex'));
+}
+
+module.exports = { htlc, serializeScript, toPaddedHexString, addPadding, add_varInt, script_to_p2sh_addr };
---
+++ src/bitcoin-contracts/redeem_htlc.js
@@ -0,0 +1,57 @@
+const bitcoin = require('bitcoinjs-lib');
+const TESTNET = bitcoin.networks.testnet;
+const minimist = require('minimist');
+const fs = require('fs');
+const htlc = require('./htlc');
+
+// key - WIF PRIVATE KEY
+// tx_id - INPUT TX ID
+// index - INPUT TX OUTPUT NUM
+// receiver - RECEIVER ADDRESS
+// out_value - OUTPUT VALUE
+// prev_tx - PREVIOUS TRANSACTION HEX
+// redeem - REDEEM SCRIPT
+// secret - HTLC SECRET
+args = minimist(process.argv.slice(2));
+
+
+const keyPair = bitcoin.ECPair.fromWIF(
+    args.key,
+    TESTNET
+);
+
+const psbt = new bitcoin.Psbt({network: TESTNET});
+psbt.setVersion(2); // These are defaults. This line is not needed.
+psbt.setLocktime(0); // These are defaults. This line is not needed.
+
+psbt.addInput({
+    hash: args.tx_id,
+    index: args.index,
+    sequence: 0xffffffff,
+    nonWitnessUtxo: Buffer.from(args.prev_tx, 'hex'),
+    redeemScript: Buffer.from(args.redeem, 'hex')
+});
+
+psbt.addOutput({
+    address: args.receiver,
+    value: parseInt(args.out_value, 10)
+});
+psbt.signInput(0, keyPair);
+psbt.validateSignaturesOfInput(0);
+
+const scriptSig = psbt.data.inputs[0].partialSig[0];
+
+const signature = scriptSig.signature.toString('hex');
+const pubKey = scriptSig.pubkey.toString('hex');
+const secret = args.secret;
+const redeemScript = args.redeem;
+
+const finalScriptSig = Buffer.from(htlc.serializeScript([signature, pubKey, secret, redeemScript]), 'hex');
+
+psbt.data.updateInput(0, { finalScriptSig });
+psbt.data.clearFinalizedInput(0);
+
+const tx = psbt.extractTransaction();
+
+console.log('Redeem tx hex:\n', tx.toHex())
+fs.writeFile('redeem.tx', tx.toHex(), (err) => { console.log(err); });
